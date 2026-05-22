36e406eb1c6c8f279f72a54ebd4374b591fc6418
Nesanesis <orionvega2343@gmail.com>
2026-05-22 22:22:32 +0300

    feat: ad db connect

--- Internal/db/db.go
+++ Internal/db/db.go
@@ -1,1 +1,17 @@
 package db
+
+import (
+	"TrackerBot/Internal/config"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func ConnDB(c *config.Config) (*sqlx.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s", c.DB.Host, c.DB.Port, c.DB.User, c.DB.Name, c.DB.Password, c.DB.SslMode)
+	db, err := sqlx.Connect("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
