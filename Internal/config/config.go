821db63c0ff96001d23467a3247d2e06674241ef
Nesanesis <orionvega2343@gmail.com>
2026-05-22 20:02:15 +0300

    feat: Add config

--- Internal/config/config.go
+++ Internal/config/config.go
@@ -1,1 +1,36 @@
 package config
+
+import (
+	"log"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
+type DB struct {
+	Port     int    `yaml:"port"`
+	Host     string `yaml:"host"`
+	User     string `yaml:"user"`
+	Name     string `yaml:"name"`
+	Password string `env:"DB_PASS"`
+	SslMode  string `yaml:"ssl_mode"`
+}
+
+type Config struct {
+	Token string `env:"token"`
+	DB    DB     `yaml:"db"`
+}
+
+func MustLoadConfig() *Config {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var c Config
+
+	err = cleanenv.ReadConfig("config.yml", &c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &c
+}
