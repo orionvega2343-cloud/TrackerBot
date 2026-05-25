fd4575cbce20a27515061cda323bc8dfb5c13453
Nesanesis <orionvega2343@gmail.com>
2026-05-26 00:38:27 +0300

    feat: add initialize bot

--- Internal/handlers/habitsHandler.go
+++ Internal/handlers/habitsHandler.go
@@ -1,1 +1,11 @@
 package handlers
+
+import "TrackerBot/Internal/service"
+
+type HabitsHandler struct {
+	s *service.HabitsService
+}
+func NewHabitsHandler(s *service.HabitsService) *HabitsHandler {
+	return &HabitsHandler{s: s}
+}
+
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"TrackerBot/Internal/config"
+	"TrackerBot/Internal/db"
+	"log"
+	"time"
+
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	cfg := config.MustLoadConfig()
+	database, err := db.ConnDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Initialize bot
+	pref := tele.Settings{
+		Token:  cfg.Token,
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b.Start()
+}
